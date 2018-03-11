0c2824dd48a24effd6866c8d8f32703c78206f88
Harvey Atwal <atwalharvey93@gmail.com>
2018-03-11 13:43:19 -0700

    JWT cookie credentials

--- src/core/api.js
+++ src/core/api.js
@@ -4,16 +4,13 @@ const api = axios.create({
   baseURL: process.env.REACT_APP_API_HOST
 });
 
+api.defaults.withCredentials = true;
+
 api.interceptors.request.use((config) => {
   const { noAuth } = config;
   if(noAuth) {
     return config;
   }
-
-  // set JWT token
-  config.headers.common = {
-    Authorization: `JWT ${localStorage.getItem("jwt_token")}`
-  };
 
   return config;
 }, (error) => {
